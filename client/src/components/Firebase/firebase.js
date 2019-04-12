d4f577182abf736f9c3f5c04beffbbfae2a55e7a
Diaconu Dan <diaconudann@gmail.com>
2019-04-12 20:49:37 +0200

    Added initial firebase context and provider

---
+++ client/src/components/Firebase/firebase.js
@@ -0,0 +1,11 @@
+import app from 'firebase/app';
+import {config} from './config' ;
+
+
+class Firebase {
+    constructor() {
+      app.initializeApp(config);
+    }
+}
+
+export default Firebase;
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import Firebase, { FirebaseContext } from './components/Firebase/index';
 import './index.css';
 import App from './App/App';
 
 render((
+    <FirebaseContext.Provider value={new Firebase()}>
     <BrowserRouter>
         <App/>
     </BrowserRouter>
+    </FirebaseContext.Provider>
 ), document.getElementById('root'));
