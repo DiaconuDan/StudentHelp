3efcffc6b19067ad251b9c51dd2afac0e6276a9e
Diaconu Dan <diaconudann@gmail.com>
2019-04-12 20:59:07 +0200

    Added signup/signIn/signOut/pwUd functions

--- client/src/components/Firebase/firebase.js
+++ client/src/components/Firebase/firebase.js
@@ -1,11 +1,26 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 import {config} from './config' ;
 
 
 class Firebase {
     constructor() {
       app.initializeApp(config);
+
+      this.auth = app.auth();
     }
+
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () =>
+         this.auth.signOut();
+
+    doPasswordUpdate = password =>
+         this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
