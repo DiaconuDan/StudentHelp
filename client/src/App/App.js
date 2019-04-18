9cab64dcbd7e89a1818cca110b966bcbc3254060
Diaconu Dan <diaconudann@gmail.com>
2019-04-18 15:59:15 +0200

    Refactored. Addedsession as HoC

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from '../pages/Home';
@@ -8,19 +8,11 @@ import { SignUp } from '../components/SignUp/SignUp' ;
 import { SignIn } from '../components/SignIn/SignIn' ;
 import Profile from '../components/Profile/Profile';
 import * as ROUTES from '../constants/routes';
+import { withAuthentication } from '../components/Session/index';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      authUser: null,
-    };
-  }
-  render() {
-    const App = () => (
+const App = () => (
       <div>
-        <Navigation authUser={this.state.authUser} />
+        <Navigation />
         <Switch>
           <Route exact path={ROUTES.LANDING} component={Home}/>
           <Route path={ROUTES.LIST} component={List}/>
@@ -29,13 +21,7 @@ class App extends Component {
           <Route path={ROUTES.PROFILE} component={Profile} />
         </Switch>
       </div>
-    )
-    return (
-      <Switch>
-        <App/>
-      </Switch>
-    );
-  }
-}
+);
 
-export default App;
+
+export default withAuthentication(App);
--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -3,14 +3,21 @@ import { Link } from 'react-router-dom';
 import SignOutButton from '../SignOut/SignOut';
 
 import * as ROUTES from '../../constants/routes';
+import { AuthUserContext } from '../Session/index';
 
-const Navigation = ({ authUser }) => (
-  <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+const Navigation = () => (
+  <div>
+    <AuthUserContext.Consumer>
+      {authUser =>
+        authUser ? <NavigationAuth /> : <NavigationNonAuth />
+      }
+    </AuthUserContext.Consumer>
+  </div>
 );
 
 const NavigationAuth = () => (
   <ul>
-      <li>
+    <li>
         <Link to={ROUTES.LANDING}>Landing</Link>
       </li>
       <li>
@@ -20,30 +27,27 @@ const NavigationAuth = () => (
         <Link to={ROUTES.PROFILE}>Profile</Link>
       </li>
       <li>
-        <Link to={ROUTES.LIST}>List</Link>
-      </li>
-      <li>
         <SignOutButton />
       </li>
-  </ul>
-
+</ul>
 );
 
 const NavigationNonAuth = () => (
   <ul>
-  <li>
-    <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-  </li>
-  <li>
-    <Link to={ROUTES.LANDING}>Landing</Link>
-  </li>
-  <li>
-    <Link to={ROUTES.HOME}>Home</Link>
-  </li>
-  <li>
-    <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-  </li>
-</ul>
+    <li>
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+      </li>
+
+      <li>
+        <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+      </li>
+
+      <li>
+        <Link to={ROUTES.HOME}>Home</Link>
+      </li>
+  </ul>
 );
+
+
 
 export default Navigation;
