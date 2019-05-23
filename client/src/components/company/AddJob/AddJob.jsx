139bed4bd600e1041949b76ed540faffbb7c0948
diaconudann <dan.diaconu@zenjob.com>
2019-05-23 12:50:42 +0200

    Added a basic, non functional addJob form

--- client/src/components/company/AddJob/AddJob.jsx
+++ client/src/components/company/AddJob/AddJob.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React from "react";
 import { compose } from "recompose";
-import { withFirebase } from "../../general/Firebase" ;
-import { withAuthorization } from '../../general/Session';
+import { withFirebase } from "../../general/Firebase";
+import { withAuthorization } from "../../general/Session";
+import { Wrapper, Email, Button, Box } from "../../general/SignIn/SignIn";
 
 const AddJob = () => (
-  <div>
-   <h1 style={{textAlign: "center"}}> Here the company will be adding a job</h1>
-  </div>
+  <Wrapper style={{ marginTop: 200 }}>
+    <div>
+      <Box>
+        <h1>Add Job</h1>
+        <form>
+          <Email name="fullName" type="text" placeholder="Location" />
+          <Email name="email" type="text" placeholder="Number of students" />
+          <Email name="email" type="text" placeholder="Payment per hour" />
+          <Email name="email" type="text" placeholder="StartDate" />
+          <Email name="email" type="text" placeholder="EndDate" />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Box>
+    </div>
+  </Wrapper>
 );
 
 const condition = authUser => authUser && authUser.role === "COMPANY";
 
-
 export default compose(
   withAuthorization(condition),
-  withFirebase,
+  withFirebase
 )(AddJob);
