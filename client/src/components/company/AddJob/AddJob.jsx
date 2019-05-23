98c06e47a5b555b7a6c857ecb33c8b4ab686e6e1
diaconudann <dan.diaconu@zenjob.com>
2019-05-23 18:42:58 +0200

    wip

--- client/src/components/company/AddJob/AddJob.jsx
+++ client/src/components/company/AddJob/AddJob.jsx
@@ -1,26 +1,107 @@
-import React from "react";
 import { compose } from "recompose";
+import React, { Component } from "react";
 import { withFirebase } from "../../general/Firebase";
 import { withAuthorization } from "../../general/Session";
 import { Wrapper, Email, Button, Box } from "../../general/SignIn/SignIn";
 
-const AddJob = () => (
-  <Wrapper style={{ marginTop: 200 }}>
-    <div>
-      <Box>
-        <h1>Add Job</h1>
-        <form>
-          <Email name="fullName" type="text" placeholder="Location" />
-          <Email name="email" type="text" placeholder="Number of students" />
-          <Email name="email" type="text" placeholder="Payment per hour" />
-          <Email name="email" type="text" placeholder="StartDate" />
-          <Email name="email" type="text" placeholder="EndDate" />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Box>
-    </div>
-  </Wrapper>
-);
+const INITIAL_STATE = {
+  location: "",
+  studentsNumber: "",
+  hourlyPayment: "",
+  startDate: "",
+  error: null
+};
+
+class AddJob extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    const {
+      location,
+      hourlyPayment,
+      startDate,
+      startHour,
+      endingHour
+    } = this.state;
+
+    this.setState({ ...INITIAL_STATE });
+    event.target.reset();
+    console.warn("intrat");
+    console.warn(this.state);
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const {
+      location,
+      hourlyPayment,
+      startDate,
+      startHour,
+      endingHour
+    } = this.state;
+
+    return (
+      <Wrapper style={{ marginTop: 150 }}>
+        <div>
+          <Box>
+            <h1>Add Job</h1>
+            <form onSubmit={this.onSubmit}>
+              <Email
+                name="location"
+                type="text"
+                placeholder="Location"
+                onChange={this.onChange}
+              />
+              <Email
+                name="studentsNumber"
+                type="text"
+                placeholder="Number of students needeed"
+              />
+              <Email
+                name="hourlyPayment"
+                type="text"
+                placeholder="Payment per hour"
+                onChange={this.onChange}
+              />
+
+              <Email
+                name="startDate"
+                type="text"
+                placeholder="Date"
+                onFocus={e => (e.target.type = "date")}
+                onChange={this.onChange}
+              />
+              <Email
+                name="startHour"
+                type="text"
+                placeholder="Starting hour"
+                onFocus={e => (e.target.type = "time")}
+                onChange={this.onChange}
+              />
+              <Email
+                name="endingHour"
+                type="text"
+                placeholder="Ending hour"
+                onFocus={e => (e.target.type = "time")}
+                onChange={this.onChange}
+              />
+
+              <Button type="submit">Submit</Button>
+            </form>
+          </Box>
+        </div>
+      </Wrapper>
+    );
+  }
+}
 
 const condition = authUser => authUser && authUser.role === "COMPANY";
 
